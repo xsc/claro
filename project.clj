586495f3dd169ff818444bdf217d2c0fd761b257
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-12-14 17:28:43 +0100

    initial commit.

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject claro "0.1.0-SNAPSHOT"
+  :description "claro que sí"
+  :url "https://github.com/xsc/claro"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"
+            :year 2015
+            :key "mit"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [potemkin "0.4.2"]
+                 [manifold "0.1.1"]
+                 [prismatic/schema "1.0.4"]]
+  :pedantic? :abort)
