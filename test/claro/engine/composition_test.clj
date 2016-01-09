892554be915012b57c4d621b1421059171ba6dd8
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-01-09 16:07:00 +0100

    add test for conditional composition.

--- test/claro/engine/composition_test.clj
+++ test/claro/engine/composition_test.clj
@@ -45,3 +45,23 @@
           value (data/then resolvable (juxt identity pr-str))
           [result printed] @(run! value)]
       (= (pr-str resolvable) printed))))
+
+(defspec t-conditional-composition 50
+  (prop/for-all
+    [resolvable0 gen-resolvable
+     resolvable1 gen-resolvable]
+    (let [run! (make-engine)
+          predicate (fn [{:keys [x y]}]
+                      (and (not (instance? Identity x))
+                           (not (instance? Identity y))
+                           (not= (class x) (class y))))
+          action (juxt :x :y)
+          value (data/on {:x resolvable0, :y resolvable1} predicate action)]
+      (if (= (-> resolvable0 :v class)  (-> resolvable1 :v class))
+        (boolean
+          (is (thrown-with-msg?
+                IllegalStateException
+                #"predicate .+ does not hold for fully resolved"
+                @(run! value))))
+        (is (= [(:v resolvable0) (:v resolvable1)]
+               @(run! value)))))))
