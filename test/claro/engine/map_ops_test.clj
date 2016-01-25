77455fc49ead98180f429e433c943a79dd8db291
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-01-25 18:23:25 +0100

    adjust map operations test to have more non-error cases.

--- test/claro/engine/map_ops_test.clj
+++ test/claro/engine/map_ops_test.clj
@@ -10,6 +10,13 @@
 
 ;; ## Generator
 
+(defn- gen-nested-map
+  [leave-gen]
+  (->> (gen/recursive-gen
+         #(gen/map gen/string-ascii %)
+         leave-gen)
+       (gen/such-that map?)))
+
 (defn- action-gen
   [k raw-op claro-op & gens]
   (->> (apply gen/tuple gens)
@@ -19,41 +26,20 @@
             :raw-action #(apply raw-op % values)
             :action #(apply claro-op % values)}))))
 
-(let [v gen/string-ascii
-      vs (gen/vector v)]
+(let [k  gen/string-ascii
+      ks (gen/vector k)
+      v  (gen/return {})]
   (def ^:private gen-actions
     (gen/vector
       (gen/one-of
-        [(action-gen 'assoc assoc data/assoc v v)
-         (action-gen 'assoc-in assoc-in data/assoc-in vs v)
-         (action-gen 'update data/update v (gen/fmap constantly v))
-         (action-gen 'update-in data/update-in vs (gen/fmap constantly v))
-         (action-gen 'select-keys data/select-keys vs)])
+        [(action-gen 'assoc assoc data/assoc k v)
+         (action-gen 'assoc-in assoc-in data/assoc-in ks v)
+         (action-gen 'update update data/update k (gen/fmap constantly v))
+         (action-gen 'update-in update-in data/update-in ks (gen/fmap constantly v))
+         (action-gen 'select-keys select-keys data/select-keys ks)])
       1 20)))
 
-(defrecord Identity [v]
-  data/Resolvable
-  (resolve! [_ _]
-    v))
-
-(def ^:private gen-nested-map
-  (->> (gen/recursive-gen
-         #(gen/map gen/string-ascii %)
-         (gen/one-of
-           [gen/string-ascii
-            (gen/fmap ->Identity gen/string-ascii)]))
-       (gen/such-that map?)))
-
 ;; ## Application
-
-(defmacro catch-it
-  [form]
-  `(try
-     (do ~form)
-     (catch ClassCastException t#
-       ::error)
-     (catch AssertionError t#
-       ::error)))
 
 (defn- reduce-actions
   [initial-value k actions]
@@ -61,14 +47,19 @@
 
 ;; ## Tests
 
+(defrecord Identity [v]
+  data/Resolvable
+  (resolve! [_ _]
+    v))
+
 (defspec t-map-ops 100
   (prop/for-all
     [actions gen-actions
-     unresolved-value gen-nested-map]
+     unresolved-value (gen-nested-map (gen/elements [{} (->Identity {})]))]
     (let [run! (make-engine)
           resolved-value @(run! unresolved-value)
-          value (catch-it (reduce-actions unresolved-value :action actions))]
+          value (reduce-actions unresolved-value :action actions)]
       (or (= value ::error)
-          (let [expected (catch-it (reduce-actions resolved-value :raw-action actions))
-                result (catch-it @(run! value))]
+          (let [expected (reduce-actions resolved-value :raw-action actions)
+                result @(run! value)]
             (= result expected))))))
