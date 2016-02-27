d3e9557d0ff663f799b03e1e056417765e98bc21
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-02-27 16:14:00 +0100

    improve generators for projection tests.

--- test/claro/engine/projection_test.clj
+++ test/claro/engine/projection_test.clj
@@ -8,36 +8,102 @@
             [claro.data :as data]
             [claro.engine.fixtures :refer [make-engine]]))
 
+;; ## Fixtures
+
 (defrecord InfiniteSeq [n]
   data/Resolvable
   (resolve! [_ _]
     {:value n
      :next (InfiniteSeq. (inc n))}))
 
-(defspec t-map-projection 100
-  (prop/for-all
-    [start-n gen/int
-     length  gen/nat]
-    (let [run! (make-engine (atom []))
-          path (concat (repeat length :next) [:value])
-          projection-template (assoc-in {:value nil} path nil)
-          value (data/project (InfiniteSeq. start-n) projection-template)
-          result @(run! value)]
-      (and (is (= start-n (:value result)))
-           (is (= (+ start-n length) (get-in result path)))))))
+;; ## Generators
 
-(defspec t-sequential-projection 50
-  (prop/for-all
-    [seq-length gen/nat
-     start-n    gen/int
-     length     gen/nat]
-    (let [run! (make-engine (atom []))
-          path (concat (repeat length :next) [:value])
-          projection-template [(assoc-in {:value nil} path nil)]
-          value (data/project
-                  (repeat seq-length (InfiniteSeq. start-n))
-                  projection-template)
-          result @(run! value)]
-      (and (is (= seq-length (count result)))
-           (is (every? #(= (:value %) start-n) result))
-           (is (every? #(=  (get-in % path) (+ start-n length)) result))))))
+(defn gen-infinite-seq
+  []
+  (gen/fmap ->InfiniteSeq gen/int))
+
+(defn- gen-leaf-template
+  []
+  (gen/one-of
+    [(gen/return nil)
+     (gen/fmap
+       #(hash-map
+          :leaf?    true
+          :valid?   false
+          :depth    0
+          :template %)
+       (gen/elements [nil data/leaf]))]))
+
+(defn- valid-next-template?
+  [t]
+  (or (nil? t)
+      (and (:valid? t) (not (:leaf? t)))))
+
+(defn- assoc-template
+  [t k v]
+  (if v
+    (assoc t k (:template v))
+    t))
+
+(defn gen-template
+  [valid?]
+  (->> (gen/recursive-gen
+         (fn [template-gen]
+           (gen/let [value-template (gen-leaf-template)
+                     next-template  template-gen]
+             (let [new-depth ((fnil inc 0) (:depth next-template))]
+               (gen/return
+                 {:leaf?    false
+                  :valid?   (and (< new-depth 256)
+                                 (valid-next-template? next-template))
+                  :depth    new-depth
+                  :template (-> {}
+                                (assoc-template :value value-template)
+                                (assoc-template :next next-template))}))))
+         (gen-leaf-template))
+       (gen/fmap #(dissoc % :leaf?))
+       (#(gen/such-that
+           (if valid? :valid? (complement :valid?))
+           %
+           50))
+       (gen/fmap :template)))
+
+(defn- gen-valid-template
+  []
+  (gen-template true))
+
+(defn- gen-invalid-template
+  []
+  (gen-template false))
+
+;; ## Tests
+
+(defn- compare-to-template
+  [value template expected-value]
+  (and (= (set (keys value))
+          (set (keys template)))
+       (or (not (contains? template :value))
+           (= (:value value) expected-value))
+       (or (not (contains? template :next))
+           (recur (:next value) (:next template) (inc expected-value)))))
+
+(defspec t-map-projection 200
+  (let [run! (make-engine)]
+    (prop/for-all
+      [template (gen-valid-template)
+       value    (gen-infinite-seq)]
+      (let [projected-value (data/project value template)
+            result @(run! projected-value)]
+        (compare-to-template result template (:n value))))))
+
+(defspec t-sequential-projection 200
+  (let [run! (make-engine)]
+    (prop/for-all
+      [template (gen-valid-template)
+       values   (gen/vector (gen-infinite-seq))]
+      (let [projected-values (data/project values [template])
+            results @(run! projected-values)]
+        (empty?
+          (for [[result {:keys [n]}] (map vector results values)
+                :when (not (compare-to-template result template n))]
+            result))))))
